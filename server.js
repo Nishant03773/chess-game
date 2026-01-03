a68ac92a2189f264b87e6e037ed69e7d444dac5f
Nishant03773 <nishantrishi039@gmail.com>
2026-01-04 00:43:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-// Serve files from the root directory instead of /public
+// Serve files from the main folder
 app.use(express.static(__dirname));
 
-// Explicitly serve index.html when someone visits the site
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -18,7 +17,7 @@ app.get('/', (req, res) => {
 const rooms = {};
 
 io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
+    console.log('User connected:', socket.id);
 
     socket.on('create-room', (name) => {
         const roomId = Math.random().toString(36).substring(2, 7).toUpperCase();
@@ -28,10 +27,10 @@ io.on('connection', (socket) => {
     });
 
     socket.on('join-room', ({ roomId, name }) => {
-        const room = rooms[roomId];
+        const room = rooms[roomId.toUpperCase()];
         if (room && room.players.length === 1) {
             room.players.push({ id: socket.id, name, color: 'black' });
-            socket.join(roomId);
+            socket.join(roomId.toUpperCase());
             io.to(room.players[0].id).emit('start-game', { opponent: name, color: 'white' });
             io.to(socket.id).emit('start-game', { opponent: room.players[0].name, color: 'black' });
         } else {
@@ -40,15 +39,11 @@ io.on('connection', (socket) => {
     });
 
     socket.on('move', ({ roomId, moveData }) => {
-        socket.to(roomId).emit('remote-move', moveData);
+        socket.to(roomId.toUpperCase()).emit('remote-move', moveData);
     });
 
     socket.on('chat', ({ roomId, msg }) => {
-        socket.to(roomId).emit('remote-chat', msg);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
+        socket.to(roomId.toUpperCase()).emit('remote-chat', msg);
     });
 });
 
